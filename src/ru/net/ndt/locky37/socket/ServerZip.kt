976714bec6ab3e40ce99e283a4e96d7b980908ba
Michail Neger <locky37@gmail.com>
2019-08-29 17:08:21 +0300

    Server accept concurrent client Catch exception main Delete function

--- src/ru/net/ndt/locky37/socket/ServerZip.kt
+++ src/ru/net/ndt/locky37/socket/ServerZip.kt
@@ -1,36 +1,75 @@
 package ru.net.ndt.locky37.socket
 
 import java.io.*
+import java.lang.Exception
 import java.net.ServerSocket
 import java.net.Socket
 import kotlin.system.exitProcess
+import java.io.DataOutputStream
+import java.io.DataInputStream
+
 
 class ServerZip {
 
     /*    @Throws(IOException::class)*/
     fun serverZip(dirDst: String, port: Int) {
 
-        val socket: Socket?
+        var serverSocket: ServerSocket? = null
+        var socket: Socket? = null
+
+        try {
+            serverSocket = ServerSocket(port)
+        } catch (e: IOException) {
+            println("Address use")
+            exitProcess(0)
+        }
+
+        while (true) {
+            try {
+                socket = serverSocket.accept()
+                println("A new client is connected : " + socket);
+
+                // obtaining input and out streams
+                //val dataInputStream = DataInputStream(socket.getInputStream())
+                val bufferedInputStream = BufferedInputStream(socket.getInputStream())
+                val dataInputStream = DataInputStream(bufferedInputStream)
+                //val dataOutputStream = DataOutputStream(socket.getOutputStream())
+
+                println("Assigning new thread for this client")
+
+                // create a new thread object
+                val t = ClientHandler(socket, dirDst, dataInputStream)
+
+                // Invoking the start() method
+                t.start()
+                t.join()
+
+            } catch (e: Exception) {
+                socket?.close()
+            }
+        }
+
+/*        val socket: Socket?
         var bufferedInputStream: BufferedInputStream? = null
         var dataInputStream: DataInputStream? = null
 
         var fileOutputStream: FileOutputStream
         var bufferedOutputStream: BufferedOutputStream?
 
-        var serverSocket: ServerSocket? = null
+        var serverSocket: ServerSocket? = null*/
 
-        try {
+/*        try {
             try {
                 serverSocket = ServerSocket(port)
             } catch (e: IOException) {
                 println("Address use")
                 exitProcess(0)
-            }
+            }*/
 
-            //ServerSocket serverSocket = new ServerSocket(port);
-            socket = serverSocket.accept()
+        //ServerSocket serverSocket = new ServerSocket(port);
+        //socket = serverSocket.accept()
 
-            bufferedInputStream = BufferedInputStream(socket.getInputStream())
+/*            bufferedInputStream = BufferedInputStream(socket.getInputStream())
             dataInputStream = DataInputStream(bufferedInputStream)
 
             val filesCount = dataInputStream.readInt()
@@ -55,14 +94,13 @@ class ServerZip {
                     bufferedOutputStream.close()
                     fileOutputStream.close()
                     serverSocket.close()
-                }
-            }
-        } finally {
-            bufferedInputStream?.close()
+                }*/
+/*            }
+        } finally {*/
+/*            bufferedInputStream?.close()
             dataInputStream?.close()
-            serverSocket?.close()
-        }
+            serverSocket?.close()*/
     }
+//    }
 }
-
 
